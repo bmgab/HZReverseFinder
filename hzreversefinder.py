9e0eed24fe37be56c0e39c7637c8f8ae268778f4
bmgab <98316393+bmgab@users.noreply.github.com>
2022-01-24 10:27:54 +0000

    Create hzreversefinder.py

---
+++ hzreversefinder.py
@@ -0,0 +1,10 @@
+
+hz = input(float())
+c = input(float())
+rb = input(float())
+o = 1 / float(hz)
+t = float(o) / float(c)
+th = float(t) / 0.7
+rarb = 2 * rb 
+ra = float(th) - float(rb)
+print(ra)
